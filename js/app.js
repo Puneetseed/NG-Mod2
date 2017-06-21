3d251d7f72bad52231888534b8d3c2cd5a1e6c83
Puneet Jindal <puneet.jindal@gmail.com>
2017-06-21 14:54:57 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,55 @@
+(function() {
+  'use strict';
+  angular
+    .module('ShoppingListCheckOff', [])
+    .controller('ToBuyShoppingController', ToBuyShoppingController)
+    .controller('AlreadyBoughtShoppingController', AlreadyBoughtShoppingController)
+    .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+  ToBuyShoppingController.$inject = ['ShoppingListCheckOffService'];
+  function ToBuyShoppingController(ShoppingListCheckOffService) {
+    var controller = this;
+
+    controller.BuyItems = ShoppingListCheckOffService.getToBuyItems();
+
+    controller.doBuy = function(index) {
+      ShoppingListCheckOffService.doBuy(index);
+    }
+  }
+
+  AlreadyBoughtShoppingController.$inject = ['ShoppingListCheckOffService'];
+  function AlreadyBoughtShoppingController(ShoppingListCheckOffService) {
+    var controller = this;
+
+    controller.boughtItems = ShoppingListCheckOffService.getBoughtItems();
+
+  }
+
+  function ShoppingListCheckOffService() {
+    var service = this;
+
+    var toBuyItems = [
+      { name: "Grapes", quantity: 50 },
+      { name: "Banana", quantity: 5 },
+      { name: "Mango", quantity: 10 },
+      { name: "Peach", quantity: 6 },
+      { name: "Papaya", quantity: 4 },
+      { name: "Avocado", quantity: 1 }]
+    var boughtItems = [];
+
+    this.doBuy = function (index) {
+      boughtItems.push(toBuyItems[index]);
+      toBuyItems.splice(index, 1);
+    }
+
+    this.getToBuyItems = function() {
+      return toBuyItems;
+    }
+
+    this.getBoughtItems = function() {
+      return boughtItems;
+    }
+  }
+
+}
+)();
